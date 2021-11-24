d9da13dbf4dd87971289879220d504969c588e54
takurooper <yourname@example.com>
2021-11-24 15:41:21 +0900

    post and change to df

---
+++ app.py
@@ -0,0 +1,44 @@
+# coding: UTF-8
+import os
+import sys
+from flask import Flask, request, jsonify , render_template, session, redirect
+import pandas as pd
+from pandas import json_normalize
+from pylibrary import DSMClustering
+from pylibrary import DSMPartitioning
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False  #JSONでの日本語文字化け対策
+
+@app.route("/hello")
+def hello():
+    return "Hello world!"
+
+@app.route('/<int:baz>', methods=['POST'])
+def post_foobar(baz):
+    return 'you posted {}\n'.format(baz)
+
+@app.route('/analyze/test', methods=['GET'])
+def get_json_from_dictionary():
+    dic = {
+        'foo': 'bar',
+        'ほげ': 'ふが'
+    }
+    return jsonify(dic)  # JSONをレスポンス
+
+@app.route('/analyze/test/json', methods=['POST'])
+def post_json():
+    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+    return jsonify(jsonData)  # JSONをレスポンス
+
+@app.route('/analyze/test/dataframe', methods=['POST'])
+def post_dataframe():
+    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+    input_df = json_normalize(jsonData) #Results contain the required data
+    #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
+    print(input_df, file=sys.stderr)
+    return "Input Dataframe successful"  # Dataframeをレスポンス
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
