a45b028164993be9945ff18e54f24535bde92e78
unknown <taku.kuro@gmail.com>
2021-11-24 13:41:15 +0900

    different api functions

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False  # JSONでの日本語文字化け対策
 
-@app.route("/")
-
+@app.route("/hello")
 def hello():
     return "Hello world!"
+
+@app.route('/<int:baz>', methods=['POST'])
+def post_foobar(baz):
+    return 'you posted {}\n'.format(baz)
+
+@app.route('/analyze/test', methods=['GET'])
+def get_json_from_dictionary():
+    dic = {
+        'foo': 'bar',
+        'ほげ': 'ふが'
+    }
+    return jsonify(dic)  # JSONをレスポンス
+
+@app.route('/analyze/test/post', methods=['POST'])
+def post_json():
+    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+    return jsonify(jsonData)  # JSONをレスポンス
+
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
