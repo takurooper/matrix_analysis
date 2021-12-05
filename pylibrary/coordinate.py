b8c4bb68d5c7e36a56814878adc987d64715f366
unknown <taku.kuro@gmail.com>
2021-12-06 00:37:14 +0900

    change tradeoff and coordinate

--- pylibrary/coordinate.py
+++ pylibrary/coordinate.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pandas as pd
 
-def solve_coordinate_matrix(sample_df):
-  dp_list = list(sample_df.columns)
-  coordinate_df = pd.DataFrame(index = dp_list, columns=dp_list)
-  for df_y in dp_list:
-    for df_x in dp_list:
-      if df_x==df_y:
-        coordinate_df.loc[df_x,df_y] = "-"
-      else:
-        if sum(np.array(sample_df[df_x]) * np.array(sample_df[df_y])) > 0:
-          coordinate_df.loc[df_x,df_y] = "○"
-        elif sum(np.array(sample_df[df_x]) * np.array(sample_df[df_y])) < 0:
-          coordinate_df.loc[df_x,df_y] = "●"
-        else:
-          coordinate_df.loc[df_x,df_y] = ""
-  return coordinate_df
+def solve_coordinate_matrix(df):
+    fngp = (df.iloc[:,1:].T*list(df.iloc[:,0])).T/81
+    fn_list = fngp.index
+    list_2d = [[0] * len(fngp.columns) for i in range(len(fngp.columns))]
+    for index in range(len(list_2d)):
+        for row in range(len(list_2d)):
+            if index==row:
+                list_2d[index][row]=1
+            elif index > row:
+                #index=1 gp2, row=0 gp1
+                dotproduct = np.dot(list(fngp.iloc[index,:]),list(fngp.iloc[row,:]))
+                list_2d[index][row] = dotproduct
+                list_2d[row][index] = dotproduct
+    max_num_in_list = max(list(map(lambda x: max(x), list_2d)))
+    fnfn = pd.DataFrame(list_2d,index=fn_list,columns=fn_list)
+    return fnfn/max_num_in_list
--- pylibrary/tradeoff.py
+++ pylibrary/tradeoff.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pandas as pd
 
-def solve_tradeoff_matrix(sample_df):
-  fr_list = list(sample_df.index)
-  tradeoff_df = pd.DataFrame(index = fr_list, columns=fr_list)
-  for df_y in fr_list:
-    for df_x in fr_list:
-      if df_x==df_y:
-        tradeoff_df.loc[df_x,df_y] = "-"
-      else:
-        if sum(np.array(sample_df.loc[df_x]) * np.array(sample_df.loc[df_y])) > 0:
-          tradeoff_df.loc[df_x,df_y] = "○"
-        elif sum(np.array(sample_df.loc[df_x]) * np.array(sample_df.loc[df_y])) < 0:
-          tradeoff_df.loc[df_x,df_y] = "●"
-        else:
-          tradeoff_df.loc[df_x,df_y] = ""
-  return tradeoff_df
+def solve_tradeoff_matrix(df):
+    fngp = (df.iloc[:,1:].T*list(df.iloc[:,0])).T/81
+    gp_list = fngp.columns
+    list_2d = [[0] * len(fngp.columns) for i in range(len(fngp.columns))]
+    for index in range(len(list_2d)):
+        for row in range(len(list_2d)):
+            if index==row:
+                list_2d[index][row]=1
+            elif index > row:
+                #index=1 gp2, row=0 gp1
+                dotproduct = np.dot(list(fngp.iloc[:,index]),list(fngp.iloc[:,row]))
+                list_2d[index][row] = dotproduct
+                list_2d[row][index] = dotproduct
+    max_num_in_list = max(list(map(lambda x: max(x), list_2d)))
+    gpgp = pd.DataFrame(list_2d,index=gp_list,columns=gp_list)
+    return gpgp/max_num_in_list
