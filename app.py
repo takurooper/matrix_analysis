aa6cd478a1c5fc62cd205c536953e4b8e7f9b17a
takurooper <yourname@example.com>
2022-01-06 11:24:36 +0900

    modify error handling

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pandas import json_normalize
 import numpy as np
 from pylibrary import DSMClustering, DSMPartitioning, tradeoff, coordinate, utilityChange, ChangePropagation, OperationPreference
+import traceback
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False  #JSONでの日本語文字化け対策
@@ -27,12 +28,12 @@ def get_json_from_dictionary():
 
 @app.route('/test/post_json', methods=['POST'])
 def post_json():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
         return jsonify(jsonData)  # JSONをレスポンス
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/test/post_dataframe', methods=['POST'])
@@ -48,154 +49,154 @@ def post_dataframe():
 
 @app.route('/tradeoff', methods=['POST'])
 def apply_tradeoff():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
-    input_df.index = jsonData["index"]
-    input_df.columns = jsonData["columns"]
-    #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
-    print("Input Dataframe successful", file=sys.stderr)
-    print(input_df, file=sys.stderr)
-    output_df = tradeoff.solve_tradeoff_matrix(input_df)
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output_df, file=sys.stderr)
-    arrayData = output_df.values.tolist()
-    index_list = list(output_df.index)
-    columns_list = list(output_df.columns)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
+        input_df.index = jsonData["index"]
+        input_df.columns = jsonData["columns"]
+        #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
+        print("Input Dataframe successful", file=sys.stderr)
+        print(input_df, file=sys.stderr)
+        output_df = tradeoff.solve_tradeoff_matrix(input_df)
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output_df, file=sys.stderr)
+        arrayData = output_df.values.tolist()
+        index_list = list(output_df.index)
+        columns_list = list(output_df.columns)
         return jsonify({"index":index_list, "columns":columns_list, "data": arrayData})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/coordinate', methods=['POST'])
 def apply_coordinate():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
-    input_df.index = jsonData["index"]
-    input_df.columns = jsonData["columns"]
-    #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
-    print("Input Dataframe successful", file=sys.stderr)
-    print(input_df, file=sys.stderr)
-    output_df = coordinate.solve_coordinate_matrix(input_df)
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output_df, file=sys.stderr)
-    arrayData = output_df.values.tolist()
-    index_list = list(output_df.index)
-    columns_list = list(output_df.columns)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
+        input_df.index = jsonData["index"]
+        input_df.columns = jsonData["columns"]
+        #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
+        print("Input Dataframe successful", file=sys.stderr)
+        print(input_df, file=sys.stderr)
+        output_df = coordinate.solve_coordinate_matrix(input_df)
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output_df, file=sys.stderr)
+        arrayData = output_df.values.tolist()
+        index_list = list(output_df.index)
+        columns_list = list(output_df.columns)
         return jsonify({"index":index_list, "columns":columns_list, "data": arrayData})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/clustering', methods=['POST'])
 def apply_clustering():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
-    input_df.index = jsonData["index"]
-    input_df.columns = jsonData["columns"]
-    #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
-    print("Input Dataframe successful", file=sys.stderr)
-    print(input_df, file=sys.stderr)
-    output_df = DSMClustering.DSM_clustering(input_df, jsonData["pow_cc"], jsonData["pow_bid"], jsonData["pow_dep"], jsonData["times"], jsonData["max_size"], jsonData["rand"], jsonData["itc"])
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output_df, file=sys.stderr)
-    arrayData = output_df.values.tolist()
-    index_list = list(output_df.index)
-    columns_list = list(output_df.columns)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
+        input_df.index = jsonData["index"]
+        input_df.columns = jsonData["columns"]
+        #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
+        print("Input Dataframe successful", file=sys.stderr)
+        print(input_df, file=sys.stderr)
+        output_df = DSMClustering.DSM_clustering(input_df, jsonData["pow_cc"], jsonData["pow_bid"], jsonData["pow_dep"], jsonData["times"], jsonData["max_size"], jsonData["rand"], jsonData["itc"])
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output_df, file=sys.stderr)
+        arrayData = output_df.values.tolist()
+        index_list = list(output_df.index)
+        columns_list = list(output_df.columns)    
         return jsonify({"index":index_list, "columns":columns_list, "data": arrayData})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/partitioning', methods=['POST'])
 def apply_partitioning():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
-    input_df.index = jsonData["index"]
-    input_df.columns = jsonData["columns"]
-    #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
-    print("Input Dataframe successful", file=sys.stderr)
-    print(input_df, file=sys.stderr)
-    output_df = DSMPartitioning.DSM_partitioning(input_df)
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output_df, file=sys.stderr)
-    arrayData = output_df.values.tolist()
-    index_list = list(output_df.index)
-    columns_list = list(output_df.columns)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
+        input_df.index = jsonData["index"]
+        input_df.columns = jsonData["columns"]
+        #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
+        print("Input Dataframe successful", file=sys.stderr)
+        print(input_df, file=sys.stderr)
+        output_df = DSMPartitioning.DSM_partitioning(input_df)
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output_df, file=sys.stderr)
+        arrayData = output_df.values.tolist()
+        index_list = list(output_df.index)
+        columns_list = list(output_df.columns)
         return jsonify({"index":index_list, "columns":columns_list, "data": arrayData})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/utility-change', methods=['POST'])
 def apply_utilityChange():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    df1 = pd.DataFrame(jsonData["data_1"]) #Results contain the required data
-    df2 = pd.DataFrame(jsonData["data_2"])
-    df3 = pd.DataFrame(jsonData["data_3"])
-    df4 = pd.DataFrame(jsonData["data_4"])
-    output = utilityChange.utility_change(df1, df2, df3, df4)
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output, file=sys.stderr)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        df1 = pd.DataFrame(jsonData["data_1"]) #Results contain the required data
+        df2 = pd.DataFrame(jsonData["data_2"])
+        df3 = pd.DataFrame(jsonData["data_3"])
+        df4 = pd.DataFrame(jsonData["data_4"])
+        output = utilityChange.utility_change(df1, df2, df3, df4)
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output, file=sys.stderr)
         return jsonify({"data": output})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/change-propagation', methods=['POST'])
 def apply_changePropagation():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
-    input_df.index = jsonData["index"]
-    input_df.columns = jsonData["columns"]
-    #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
-    print("Input Dataframe successful", file=sys.stderr)
-    print(input_df, file=sys.stderr)
-    output_df = ChangePropagation.Change_propagation_df_DSM(input_df)
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output_df, file=sys.stderr)
-    arrayData = output_df.values.tolist()
-    index_list = list(output_df.index)
-    columns_list = list(output_df.columns)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        input_df = pd.DataFrame(jsonData["data"]) #Results contain the required data
+        input_df.index = jsonData["index"]
+        input_df.columns = jsonData["columns"]
+        #return render_template('simple.html',  tables=[input_df.to_html(classes='data')], titles=input_df.columns.values)
+        print("Input Dataframe successful", file=sys.stderr)
+        print(input_df, file=sys.stderr)
+        output_df = ChangePropagation.Change_propagation_df_DSM(input_df)
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output_df, file=sys.stderr)
+        arrayData = output_df.values.tolist()
+        index_list = list(output_df.index)
+        columns_list = list(output_df.columns)    
         return jsonify({"index":index_list, "columns":columns_list, "data": arrayData})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 @app.route('/operation-preference', methods=['POST'])
 def operation_preference():
-    jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-    data_1 = jsonData["data_1"]
-    data_1_columns = data_1.pop(0)
-    df_qfd = pd.DataFrame(data_1, columns=data_1_columns) #Results contain the required data
-    data_2 = jsonData["data_2"]
-    data_2_columns = data_2.pop(0)
-    df_fn_imp = pd.DataFrame(data_2, columns=data_2_columns)
-    data_3 = jsonData["data_3"]
-    data_3_columns = data_3.pop(0)
-    df_dp_precon = pd.DataFrame(data_3, columns=data_3_columns)
-    output_df = OperationPreference.main(df_qfd, df_fn_imp, df_dp_precon, jsonData["calc_type"])
-    print("Analyzing Dataframe successful", file=sys.stderr)
-    print(output_df, file=sys.stderr)
-    arrayData = output_df.values.tolist()
-    index_list = list(output_df.index)
-    columns_list = list(output_df.columns)
     try:
+        jsonData = request.get_json(force=True)  # POSTされたJSONを取得
+        data_1 = jsonData["data_1"]
+        data_1_columns = data_1.pop(0)
+        df_qfd = pd.DataFrame(data_1, columns=data_1_columns) #Results contain the required data
+        data_2 = jsonData["data_2"]
+        data_2_columns = data_2.pop(0)
+        df_fn_imp = pd.DataFrame(data_2, columns=data_2_columns)
+        data_3 = jsonData["data_3"]
+        data_3_columns = data_3.pop(0)
+        df_dp_precon = pd.DataFrame(data_3, columns=data_3_columns)
+        output_df = OperationPreference.main(df_qfd, df_fn_imp, df_dp_precon, jsonData["calc_type"])
+        print("Analyzing Dataframe successful", file=sys.stderr)
+        print(output_df, file=sys.stderr)
+        arrayData = output_df.values.tolist()
+        index_list = list(output_df.index)
+        columns_list = list(output_df.columns)
         return jsonify({"index":index_list, "columns":columns_list, "data": arrayData})
-    except Exception as e:
+    except Exception:
         return {
-                    'error': "{}".format(e)
+                    'message': "{}".format(traceback.format_exc()),
                 }
 
 if __name__ == "__main__":
