8007d57441f22036cdb645b19c357b526178c0fb
takurooper <yourname@example.com>
2022-01-07 14:25:15 +0900

    fix bug 2 coordinate and tradeoff diff func

--- app.py
+++ app.py
@@ -205,10 +205,10 @@ def solve_coordinate_diff():
         jsonData = request.get_json(force=True)  # POSTされたJSONを取得
         data_1 = jsonData["data_1"]
         data_1_columns = data_1.pop(0)
-        df1 = pd.DataFrame(data_1, columns=data_1_columns) #Results contain the required data
+        df1 = pd.DataFrame(data_1, columns=data_1_columns).set_index('背反') #Results contain the required data
         data_2 = jsonData["data_2"]
         data_2_columns = data_2.pop(0)
-        df2 = pd.DataFrame(data_2, columns=data_2_columns)
+        df2 = pd.DataFrame(data_2, columns=data_2_columns).set_index('調整')
         output = coordinate_diff.coordinateDiff(df1, df2)
         print("Analyzing Dataframe successful", file=sys.stderr)
         print(output, file=sys.stderr)
@@ -224,10 +224,10 @@ def solve_tradeoff_diff():
         jsonData = request.get_json(force=True)  # POSTされたJSONを取得
         data_1 = jsonData["data_1"]
         data_1_columns = data_1.pop(0)
-        df1 = pd.DataFrame(data_1, columns=data_1_columns) #Results contain the required data
+        df1 = pd.DataFrame(data_1, columns=data_1_columns).set_index('背反') #Results contain the required data
         data_2 = jsonData["data_2"]
         data_2_columns = data_2.pop(0)
-        df2 = pd.DataFrame(data_2, columns=data_2_columns)
+        df2 = pd.DataFrame(data_2, columns=data_2_columns.set_index('調整'))
         output = tradeoff_diff.tradeoffDiff(df1, df2)
         print("Analyzing Dataframe successful", file=sys.stderr)
         print(output, file=sys.stderr)
