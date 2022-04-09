2a6d1cdc8764130d84265a27878cecc905d563de
unknown <taku.kuro@gmail.com>
2022-04-09 20:50:17 +0900

    header, columnの指定

--- app.py
+++ app.py
@@ -241,8 +241,8 @@ def solve_tradeoff_diff():
 def apply_system1():
     try:
         jsonData = request.get_json(force=True)  # POSTされたJSONを取得
-        df1 = pd.DataFrame(jsonData["data_1"]) #Results contain the required data
-        df2 = pd.DataFrame(jsonData["data_2"])
+        df1 = pd.DataFrame(jsonData["data_1"],header=[0,1,2,3], index_col=[0,1,2]) #Results contain the required data
+        df2 = pd.DataFrame(jsonData["data_2"],header=[0,1,2], index_col=[0,1,2])
         output = system1.main(df1, df2)
         print("Analyzing Dataframe successful", file=sys.stderr)
         print(output, file=sys.stderr)
