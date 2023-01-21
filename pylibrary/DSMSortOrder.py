14ab1ac1b9a0dbefc3b11ad5c0f8999202ac3557
takurooper <yourname@example.com>
2023-01-22 01:16:48 +0900

    edit sort

--- pylibrary/DSMSortOrder.py
+++ pylibrary/DSMSortOrder.py
@@ -1,6 +1,9 @@
 def DSM_sort_order(priority_df):
-    priority_weight = priority_df.sum() - priority_df.T.sum()
+    priority_weight = priority_df.sum()
     priority_order =  priority_weight.sort_values(ascending=True).index
+    priority_df = priority_df[priority_order].reindex(priority_order)
+    priority_weight = priority_df.T.sum()
+    priority_order =  priority_weight.sort_values(ascending=False).index
     priority_df = priority_df[priority_order].reindex(priority_order)
     priority_order = '→'.join(priority_order)
     return(priority_df, priority_order)
